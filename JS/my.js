02f5cba2acb128c6ed997b156c43ee805c11b585
syoji-haruki <j_0@docomo.ne.jp>
2019-05-06 19:11:16 +0900

    add anime
    トップページアニメーション追加

--- JS/my.js
+++ JS/my.js
@@ -1,3 +1,15 @@
-$(function() {
-  console.log('こんにちは');
+// anime
+$(function(){
+  $(".mainSite").css("display", "none");
+  setTimeout(function(){
+  $('.anime').fadeOut();
+  },3800);
+});
+
+$(function(){
+  $(".mainSite").css({opacity:'0'});
+  setTimeout(function(){
+  $(".mainSite").css("display", "block");
+  $(".mainSite").stop().animate({opacity:'1'},2500);
+  },3800);
 });
